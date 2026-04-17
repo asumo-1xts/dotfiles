31a399ce24029417cac5c579edf0d924bb2e605a
aSumo <1xtelescope@gmail.com>
2026-04-17 23:18:31 +0900

    fix: シンボリックリンクに絶対パスを使用

--- script/symbolic-link.sh
+++ script/symbolic-link.sh
@@ -1,11 +1,15 @@
 #!/bin/zsh
 
 rm ~/.zshrc
-rm -rf ~/.config
+rm -rf ~/.config && mkdir -p ~/.config && mkdir -p ~/.config/sheldon
+rm -rf ~/.vscode-server/data/Machine
+mkdir -p ~/.vscode-server/data/Machine
 
-ln -nfs ./.zshrc ~/.zshrc
-ln -nfs ./.config/alias.zsh ~/.config/alias.zsh
-ln -nfs ./.config/functions.zsh ~/.config/functions.zsh
-ln -nfs ./config/mise.toml ~/.config/mise.toml
-ln -nfs ./config/starship.toml ~/.config/starship.toml
-ln -nfs ./sheldon/plugins.toml ~/.config/sheldon/plugins.toml
+# 絶対パスを使用すること！
+ln -s ~/dotfiles/.zshrc ~/.zshrc
+ln -s ~/dotfiles/.config/alias.zsh ~/.config/alias.zsh
+ln -s ~/dotfiles/.config/functions.zsh ~/.config/functions.zsh
+ln -s ~/dotfiles/.config/mise.toml ~/.config/mise.toml
+ln -s ~/dotfiles/.config/starship.toml ~/.config/starship.toml
+ln -s ~/dotfiles/.config/sheldon/plugins.toml ~/.config/sheldon/plugins.toml
+ln -s ~/dotfiles/.vscode-server/data/Machine/settings.json ~/.vscode-server/data/Machine/settings.json
